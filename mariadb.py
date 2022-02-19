a1f3b08c7cc059394e065710241fc0c2f56ed441
Soldy <4786022+Soldy@users.noreply.github.com>
2022-02-19 23:05:42 +0000

    mariadb migration table
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- mariadb.py
+++ mariadb.py
@@ -5,6 +5,18 @@ import mariadb
 import sys
 import os
 
+_table_query = """
+
+create table panthera_migration (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    migration INT UNSIGNED NOT NULL,
+    file VARCHAR(256) NOT NULL,
+    hash VARCHAR(512),
+    date INT UNSIGNED NOT NULL
+    PRIMARY KEY ( id )
+);
+
+"""
 
 try:
     conn = mariadb.connect(
