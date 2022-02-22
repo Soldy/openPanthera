57645648b6779d1a8bd84ee733dc576db4e1c6ee
Soldy <4786022+Soldy@users.noreply.github.com>
2022-02-22 23:16:45 +0000

    careless logic
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- mariadb.py
+++ mariadb.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 
-
+import directory
 import mariadb
+import config
 import sys
 import os
+
+_config = config.read('mariadb')
 
 _table_query = """
 
@@ -20,15 +23,24 @@ create table panthera_migration (
 
 try:
     conn = mariadb.connect(
-        user="",
-        password="",
-        host="",
-        port=,
-        database=""
+        user=_config.user,
+        password=config.password,
+        host=config.host,
+        port=_config.port,
+        database=_config.database
     )
 except mariadb.Error as e:
     print(f"MariaDb died in my arm: {e}")
     sys.exit(1)
+
+_cur = conn.cursor()
+
+def buildTable():
+    tables = directory.readTable()
+    for table in  tables::
+        cur.execute(tables[table])
+
+
 
 def build():
 
