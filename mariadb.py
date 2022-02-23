705af2a9471b9cc833f1615e7d952e3a407e4798
Soldy <4786022+Soldy@users.noreply.github.com>
2022-02-23 23:26:14 +0000

    build functions
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- mariadb.py
+++ mariadb.py
@@ -37,10 +37,58 @@ _cur = conn.cursor()
 
 def buildTable():
     tables = directory.readTable()
-    for table in  tables::
+    for table in  tables:
         cur.execute(tables[table])
 
+def buildTableLink():
+    links = directory.readTableLink()
+    for link in  links:
+        cur.execute(links[link])
 
+def buildFunction():
+    functions = directory.readFunction()
+    for function in  functions:
+        cur.execute(functions[function])
+
+def buildProcedure():
+    procedures = directory.readProcedure()
+    for procedure in  procedures:
+        cur.execute(procedures[procedure])
+
+def buildView():
+    views = directory.readView()
+    for view in  views:
+        cur.execute(views[view])
+
+def buildIndex():
+    indexs = directory.readIndex()
+    for index in  indexs:
+        cur.execute(indexs[index])
+
+def buildForein():
+    foreins = directory.readForein()
+    for forein in  foreins:
+        cur.execute(foreins[forein])
+
+def buildMigration():
+    migrations = directory.readMigration()
+    for migration in  migrations:
+        cur.execute(migrations[migration])
+
+def buildSeed():
+    seeds = directory.readSeed()
+    for seed in  seeds:
+        cur.execute(seeds[seed])
+
+def buildEvent():
+    events = directory.readEvent()
+    for event in  events:
+        cur.execute(events[event])
+
+def buildAdmin():
+    admins = directory.readAdmin()
+    for admin in  admins:
+        cur.execute(admins[admin])
 
 def build():
 
