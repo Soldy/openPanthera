77e40b0b46f39ac4184f6fba36b8bcfb399f76ce
Soldy <4786022+Soldy@users.noreply.github.com>
2025-05-05 22:07:44 +0100

    schema docstring
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- src/openPanthera/schema.py
+++ src/openPanthera/schema.py
@@ -1,27 +1,19 @@
+"""
+Virtual schema descriptor
+"""
 import openPanthera.directory as d
 import openPanthera.mariadblib as m
 import openPanthera.migrate as e
 import openPanthera.modules as o
 import hnyconfig as config
 
-
-def schemas(ui)->list:
-    schema_list = []
-    for schema_name in o.read():
-        schema_list = VirtualSchema(
-            schema_name,
-            ui
-        )
-
 class VirtualSchema:
-    def resolv(self, main, sub):
-        return self._agents[main](sub)
-    def _init(self, sub):
-        if sub == 'sql':
-            self._mariadb.initMigrationTable()
-        if sub == 'directory':
-            self._directory.init()
+    """
+    Virtual schema manager
 
+    :param: str:
+    :param: ui:
+    """
     def __init__(self, schema:str, ui):
         self._directory = d.DirectoryClass(schema, ui)
         self._mariadb = m.MariaDbClass(ui, config, self._directory)
@@ -34,3 +26,38 @@ class VirtualSchema:
             'directory': self._directory.resolv,
             'show'     : self._mariadb.show
         }
+    def resolv(self, main, sub)->any:
+    """
+    Virtual schema agent service
+
+    :param: str:
+    :param: str:
+    :return: any:
+    """
+        return self._agents[main](sub)
+    def _init(self, sub: str):
+    """
+    Virtual schema manager
+    sub init
+
+    :param: str:
+    """
+        if sub == 'sql':
+            self._mariadb.initMigrationTable()
+        if sub == 'directory':
+            self._directory.init()
+
+
+def schemas(ui)->VirtualSchema:
+    """
+    Schema definitor
+
+    :return: VirtualSchema
+    """
+    schema_list = []
+    for schema_name in o.read():
+        schema_list = VirtualSchema(
+            schema_name,
+            ui
+        )
+
