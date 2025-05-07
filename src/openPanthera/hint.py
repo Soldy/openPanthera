a8ace3d977b87cf4ddd0bebbc6f2f093e94be2c9
Soldy <4786022+Soldy@users.noreply.github.com>
2025-05-07 09:24:34 +0100

    hint refactor
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- src/openPanthera/hint.py
+++ src/openPanthera/hint.py
@@ -5,107 +5,186 @@ We want to ensure the AI receives the correct information, so we prepare the dat
 
 
 class reader:
-  def __init__(self):
-    self._vars = {}
-    self._in = False
-    self._cols = []
-    self._args = []
-    self._tables = []
-    self._functions = []
-    self._procedurs = []
-    self._returns = []
+    """
+    AXPN reader and preparator
+    """
+    def __init__(self):
+        self._meta = {
+            "first_line":0,
+            "last_line":0
+        }
+        self._data = {
+           'vars'      : {},
+           'cols'      : [],
+           'args'      : [],
+           'tables'    : [],
+           'functions' : [],
+           'procedurs' : [],
+           'views'     : [],
+           'returns'   : []
+        }
 
-  def typeCheck(self, line)->bool:
-      
-  def _firstLineCheck(self, line_)->bool:
-    line = line_.lstrip()
-    if line[:3] == '/**':
-      self._in = True
-    else:
-      self._in = False 
+    def metaFirst(self)->int:
+        """
+        first meta line
 
-  def _lastLineCheck(self, line_)->bool:
-    line = line_.lstrip()
-    if line[:3] == '**/':
-      self._in = False
-    else:
-      self._in = True
+        :return:int:
+        """
+        return int(self._meta["first_line"])
 
-  def _dataMine(self, lines_)->None:
-    for line in lines_:
-      if self._in:
-        if self._dataLine(line) == False:
-          self._lastLineCheck(line)
-      else:
-        self._firstLineCheck(line)
-  def _dataLineVar(self, parts_)->list:
-    _type = (_parts[2].strip())
-    _name = (_parts[3].strip())
-    _additional = (''.join(_parts[3:]))
-    _col = {
-      "name" : _name,
-      "type" : _type,
-      "additional" : _additional
-    }
-    return _col
-  def _dataLineTwoNames(self, parts_)->list:
-    _name = (_parts[2].strip())
-    _additional = (''.join(_parts[2:]))
-    _two_names = {
-      "name" : _name,
-      "additional" : _additional
-    }
-    return _two_names
-  def _dataLineArgument(self, parts_)-> None:
-    self._args.append(
-      self._dataLineVar(parts_)
-    )
-  def _dataLineTable(:self, parts_)-> None:
-    self._tables.append(
-      self._dataLineTwoNames(parts_)
-    )
-  def _dataLineFunction(self, parts_)-> None:
-    self._functions.append(
-      self._dataLineTwoNames(parts_)
-    )
-  def _dataLineProcedure(self, parts_)-> None:
-    self._procedurs.append(
-      self._dataLineTwoNames(parts_)
-    )
-  def _dataLineView(self, parts_)-> None:
-    self._views.append(
-      self._dataLineTwoNames(parts_)
-    )
-  def _dataLineReturn(self, parts_)-> None:
-    self._returns.append(
-      self._dataLineVar(parts_)
-    )
-  def _dataLineCol(self, parts_)-> None:
-    self._cols.append(
-      self._dataLineVar(parts_)
-    )
-  def _dataLine(self, line_)->bool:
-    _line = line.strip()
-    if _line[:1] != '*':
-      return False
-    _parts = _line.split()
-    if len(_parts) < 3:
-      return False
-    _name = (_parts[1].strip())
-    if _name == "@pcol": 
-      self._dataLineCol(parts_)
-    elif _name == "@parg":
-      self._dataLineArgument(parts_)
-    elif _name == "@preturn":
-      self._dataLineReturn(parts_)
-    elif _name == "@ptable":
-      self._dataLineTable(parts_)
-    elif _name == "@pfunction":
-      self._dataLineFunction(parts_)
-    elif _name == "@pprocedure":
-      self._dataLineProcedure(parts_)
-    elif _name == "@pview":
-      self._dataLineView(parts_)
-    else:
-      return False
-    return True
+    def metaLast(self)->int:
+        """
+        first meta line
+
+        :return:int:
+        """
+        return int(self._meta["last_line"])
+
+    def typeCheck(self)->bool:
+        """
+        correct type check
+        """
+        return True
+
+    def _firstLineCheck(self, line_)->bool:
+        """
+        first line check
+
+        :param:str:
+        :return:bool:
+        """
+        line = line_.lstrip()
+        if line[:3] == '/**':
+            return True
+        return False
+
+    def _lastLineCheck(self, line_)->bool:
+        """
+        last line check
+
+        :param:str:
+        :return:bool:
+        """
+        line = line_.lstrip()
+        if line[:3] == '**/':
+            return True
+        return False
+
+    def dataMine(self, lines_:list[str])->None:
+        """
+        last line check
+
+        :param:list[str]:
+        """
+        serial = 0
+        in_in = False
+        for line in lines_:
+            if in_in:
+                if self._dataLine(line):
+                    if self._lastLineCheck(line):
+                        self._meta["last_line"] = int(serial)
+                        in_in = False
+            else:
+                if self._firstLineCheck(line):
+                    self._meta["first_line"] = int(serial)
+                    in_in = True
+        serial = serial + 1
+
+    def _dataLineVar(self, parts_: list[str])->dict[str,str]:
+        """
+        data line variables
+
+        :param:list[str]:
+        :param:dict[str,str]:
+        """
+        atype = parts_[2].strip()
+        name = parts_[3].strip()
+        additional = ''.join(parts_[3:])
+        col = {
+          "name" : name,
+          "type" : atype,
+          "additional" : additional
+        }
+        return col
+
+    def _dataLineTwoNames(self, parts_:list[str])->dict[str,str]:
+        """
+        two name section data line
+
+        :param:list[str]:
+        :param:dict[str,str]:
+        """
+        name = parts_[2].strip()
+        additional = ''.join(parts_[2:])
+        two_names = {
+          "name" : name,
+          "additional" : additional
+        }
+        return two_names
+
+    def _dataLineArgument(self, parts_)->None:
+        self._data['args'].append(
+          self._dataLineVar(parts_)
+        )
+
+    def _dataLineTable(self, parts_)-> None:
+        self._data['tables'].append(
+          self._dataLineTwoNames(parts_)
+        )
+
+    def _dataLineFunction(self, parts_)-> None:
+        self._data['functions'].append(
+          self._dataLineTwoNames(parts_)
+        )
+
+    def _dataLineProcedure(self, parts_)-> None:
+        self._data['procedurs'].append(
+          self._dataLineTwoNames(parts_)
+        )
+
+    def _dataLineView(self, parts_)-> None:
+        self._data['views'].append(
+          self._dataLineTwoNames(parts_)
+        )
+
+    def _dataLineReturn(self, parts_)-> None:
+        self._data['returns'].append(
+          self._dataLineVar(parts_)
+        )
+
+    def _dataLineCol(self, parts_)-> None:
+        self._data['cols'].append(
+          self._dataLineVar(parts_)
+        )
+
+    def _dataLine(self, lines_)->bool:
+        """
+        data line / meta line processor
+
+        :param:list[str]:
+        :return:bool:
+        """
+        line = lines_.strip()
+        if line[:1] != '*':
+            return False
+        parts = line.split()
+        if len(parts) < 3:
+            return False
+        name = parts[1].strip()
+        if name == "@pcol":
+            self._dataLineCol(parts)
+        elif name == "@parg":
+            self._dataLineArgument(parts)
+        elif name == "@preturn":
+            self._dataLineReturn(parts)
+        elif name == "@ptable":
+            self._dataLineTable(parts)
+        elif name == "@pfunction":
+            self._dataLineFunction(parts)
+        elif name == "@pprocedure":
+            self._dataLineProcedure(parts)
+        elif name == "@pview":
+            self._dataLineView(parts)
+        else:
+            return False
+        return True
